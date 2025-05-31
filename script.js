c2bdbe959562c99bb869e11ed7dd4ba6e5c5b4b3
Muntasir Udoy <muntasirudoy4@gmail.com>
2025-05-31 12:17:38 +0600

    submit 2666

--- script.js
+++ script.js
@@ -102,3 +102,22 @@ var filter = function (arr, fn) {
 //     return n > i;
 //   })
 // );
+
+var once = function (fn) {
+  let called = false;
+
+  return function (...args) {
+    if (!called) {
+      called = true;
+      return fn(...args);
+    } else {
+      return undefined;
+    }
+  };
+};
+
+let fn = (a, b, c) => a + b + c;
+let onceFn = once(fn);
+
+console.log(onceFn(1, 2, 3));
+console.log(onceFn(2, 3, 6));
